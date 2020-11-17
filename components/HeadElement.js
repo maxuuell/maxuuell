d45555c5795ee806a961d8526c899728c6928c2a
Maxwell DeMers <maxuuell@gmail.com>
2020-11-16 22:41:07 -0800

    Add hotjar

--- components/HeadElement.js
+++ components/HeadElement.js
@@ -16,6 +16,18 @@ const HeadElement = ({ children }) => {
       />
       <script src="https://f.convertkit.com/ckjs/ck.5.js" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2068502,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+        }}
+      />
       {children}
     </Head>
   );
